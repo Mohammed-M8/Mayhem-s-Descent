fbf8acf3c632f6865e2b0d8d6480cf9181b4f14f
Ali4lqassab <201900568@student.polytechnic.bh>
2025-05-15 03:11:49 +0300

    added main menu to the game
    problem with music

---
+++ Assets/Scripts/MainMenu/LevelLoader.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    public void LoadLevel(int levelIndex)
+    {
+        //SceneManager.LoadScene(sceneName);
+        SceneManager.LoadScene(levelIndex);
+    }
+}
